d00703b16ab4fcbbe60e4527c27afdf3be5fe005
Ahmad Dahlan Hafizh <144985531+ADAHafizh@users.noreply.github.com>
2023-12-13 21:09:00 +0400

    2.0
    - Cleaned code
    - Fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 import { Popover, initMDB } from "mdb-ui-kit";
+import Lenis from "@studio-freight/lenis";
 initMDB({ Popover });
 
+const lenis = new Lenis({
+    duration: 10,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
