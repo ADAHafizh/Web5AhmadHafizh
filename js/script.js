43772b57732f242efe07ad33fd8832787b4e8b0f
Ahmad Dahlan Hafizh <144985531+ADAHafizh@users.noreply.github.com>
2023-12-14 14:40:52 +0400

    4.0
    - Added popovers
    - Installed Node
    - Animations are working now

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 import { Popover, initMDB } from "mdb-ui-kit";
-import Lenis from "@studio-freight/lenis";
 initMDB({ Popover });
 
 import { Collapse, initMDB } from "mdb-ui-kit";
 initMDB({ Collapse });
 
+//Lenis Smooth Scrolling
+import Lenis from "@studio-freight/lenis";
 const lenis = new Lenis({
-    duration: 10,
+    duration: 1,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
 });
 
@@ -20,3 +21,8 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+//Disable Right Click -- does not work?
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+}, false);
